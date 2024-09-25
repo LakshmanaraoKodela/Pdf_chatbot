b6e21db5c193f2812e9d18cfab511d0f5d4ecb21
LakshmanaraoKodela <162315057+LakshmanaraoKodela@users.noreply.github.com>
2024-09-25 15:08:05 +0530

    Update chatpdf1.py

--- chatpdf1.py
+++ chatpdf1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import PdfReader
+from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
